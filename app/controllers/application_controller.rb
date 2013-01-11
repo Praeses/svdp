efa5145994c6034aab0828357ec637818fe25488
Lex Childs <lexchilds@gmail.com>
2013-01-11 12:43:24 -0600

    logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :layout_by_resource
   before_filter :authenticate_user!
+
+
+  def layout_by_resource
+    return 'devise' if devise_controller?
+    "application"
+  end
+
 
 end
