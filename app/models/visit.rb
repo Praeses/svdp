9572bc21e31f9b8f08ab85881407d9ac97c38282
Lex Childs <lexchilds@gmail.com>
2013-01-11 15:39:12 -0600

    adding visit

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,6 @@
 class Visit < ActiveRecord::Base
   belongs_to :person
-  attr_accessible :date_of_visit, :note
+  attr_accessible :date_of_visit, :note, :person_id
 
   scope :with_person_id, lambda { |id| where :person_id => id }
 
