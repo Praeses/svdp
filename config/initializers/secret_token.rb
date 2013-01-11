6725392e5091fedb686310a58489d1811b3c5a21
Lex Childs <lexchilds@gmail.com>
2013-01-11 11:16:43 -0600

    init the empty site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svdp::Application.config.secret_token = '6ecb22f47dd08e09fa145cb07afa4d46faa1c91964ee76f0b4e91f0bdb062dc5d6cee4996d82fe3c96b21786572235e4d3abd0d2aaf5901a1a8324ef2df8d757'
