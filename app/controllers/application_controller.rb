6e1a3516d50ae3940af0921457b0a8a4a5ef5f5b
Lex Childs <lexchilds@gmail.com>
2013-01-18 16:39:53 -0600

    cleanup for release

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout :layout_by_resource
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :redirect_https
 
 
   def layout_by_resource
     return 'devise' if devise_controller?
     "application"
+  end
+
+  def redirect_https
+    return true unless Rails.env.production?
+    redirect_to :protocol => "https://" unless request.ssl?
+    return true
   end
 
 
