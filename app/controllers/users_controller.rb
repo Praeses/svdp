d1d11e37b08749f655eddaa21700a09ab4c790d7
Lex Childs <lexchilds@gmail.com>
2013-01-22 15:40:33 -0600

    update to user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         @user.send_reset_password_instructions
-        format.html { redirect_to [:edit, @user], :notice => 'User was successfully created.' }
+        format.html { redirect_to :users, :notice => 'User was successfully updated.' }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
