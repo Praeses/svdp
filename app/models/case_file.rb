ae7bb7b96f948f2bdd4e8285a5cfd1c2969e7567
Zac <zac@kleinpeter.org>
2013-07-11 16:07:29 -0500

    Pending

--- app/models/case_file.rb
+++ app/models/case_file.rb
@@ -1,6 +1,6 @@
 class CaseFile < ActiveRecord::Base
   has_many :visits
   has_many :people, through: :visits
-  has_many :families, through: :people
   has_many :users, through: :visits
+  belongs_to :family
 end
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,5 @@
 class Family < ActiveRecord::Base
-
+  has_many :case_files
   has_many :people
   has_one :primay_addresses, as: :addressable
   has_many :addresses, as: :addressable
