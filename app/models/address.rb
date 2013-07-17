797c939739d243ad3c8526f2191bf3b46baf0446
Zac <zac@kleinpeter.org>
2013-07-17 12:13:33 -0500

    fixing search and migration

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,62 +3,12 @@ class Address < ActiveRecord::Base
 
   #attr_accessible :line_1, :line_2, :line_3, :city, :state, :zip
 
-  scope :with_line_1, ->(term) do
-    tbl = Address.arel_table
-    Address.where(tbl[:line_1].matches("%#{term}%"))
+  def to_family
+    addressable.to_family
   end
-
-  scope :with_line_2, ->(term) do
-    tbl = Address.arel_table
-    Address.where(tbl[:line_2].matches("%#{term}%"))
-  end
-
-  scope :with_line_3, ->(term) do
-    tbl = Address.arel_table
-    Address.where(tbl[:line_3].matches("%#{term}%"))
-  end
-
-  scope :with_city, ->(term) do
-    tbl = Address.arel_table
-    Address.where(tbl[:city].matches("%#{term}%"))
-  end
-
-  scope :with_state, ->(term) do
-    tbl = Address.arel_table
-    Address.where(tbl[:state].matches("%#{term}%"))
-  end
-
-  scope :with_zip, ->(term) do
-    tbl = Address.arel_table
-    Address.where(tbl[:zip].matches("%#{term}%"))
-  end
-
-  class << self
-    def search term
-      ids = with_line_1(term).pluck(:id) +
-      with_line_2(term).pluck(:id) +
-      with_line_3(term).pluck(:id) +
-      with_city(term).pluck(:id)   +
-      with_state(term).pluck(:id)  +
-      with_zip(term).pluck(:id)
-
-      find ids
-    end
-  end
-
 
   def to_s
     [line_1, line_2, line_3, city, state, zip].join(' ')
   end
 
-
-  def family_to_s
-  end
-
-  def persion_to_s
-  end
-
-  def extra_to_s
-    addressable.to_s
-  end
 end
