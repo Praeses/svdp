4697ad6bbc7e088856ae7c34e1e6d0cb7bae06d2
Zac <zac@kleinpeter.org>
2013-07-18 16:06:11 -0500

    Updating layout

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,11 @@
 module SearchesHelper
+
+  def case_file family
+    family.case_files.where(number: params[:term].to_i).first
+  end
+
+  def case_file_link family
+    cf = case_file(family)
+    link_to "Case File ##{params[:term]}", edit_case_file_url(cf), target: '_blank' if cf
+  end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -5,9 +5,17 @@ class Search < ActiveRecord::Base
   end
 
   def call
+    (main + extras).uniq
+  end
+
+  def main
     klasses.map do |model|
       model.basic_search(term).map(&:to_family)
     end.flatten.uniq
+  end
+
+  def extras
+    CaseFile.where(number: term.to_i).map(&:to_family)
   end
 
 end
